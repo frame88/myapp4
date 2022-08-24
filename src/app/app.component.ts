41920d60e8091942b04497ff4f707d0642ed7c41
frame88 <fleuzzi97@gmail.com>
2022-08-24 11:52:41 +0200

    stampa di un array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,24 @@ import { reduce } from 'rxjs';
 @Component({
   selector: 'app-root',
   template: `
+  <!-- stampa titolo -->
   <h1 class="ci">ciao</h1>
-  <input (click)="clickHandler()" type=" ">  
+
+  <!-- stampa console log -->
+  <input type="area"(click)="clickHandler()">
+
+  <!-- stamap di un array -->
+  <li
+    *ngFor="let user of users">
+    {{user}}
+  </li>
   `
 })
 export class AppComponent {
   clickHandler() {
     console.log('ciao');
   }
+  
+  users = ['Mario', 'Cosimo','Fabio'];
+
 }
